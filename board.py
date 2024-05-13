efb6f28913fe420a74990d7cdc8e6dfb99c0ebfd
thisismzz <mahdi.zeinal.zadeh.1384@gmail.com>
2024-05-13 11:15:10 +0330

    add other things to user init

---
+++ board.py
@@ -0,0 +1,9 @@
+class Project:
+    def __init__(self,name,title):
+        self.name=name
+        self.
+        
+
+
+class Task:
+    def __init__(self)
--- users.py
+++ users.py
@@ -1,19 +1,26 @@
 from enum import Enum
 import json
+import base64
+import uuid
 
 #................................
 
-class userStatus:
+class UserStatus(Enum):
     ACTIVE = 1
     DEACTIVE = 0
+    
+    def __str__(self):
+        return self.name
 
 
-class user:
+class User:
     def __init__(self,email,password,username):
         self._email=email
-        self._password=password
+        self._password=str(base64.b64encode(password.encode("utf-8")))
         self.username=username
-        self.status=userStatus.ACTIVE
+        self.status=str(UserStatus.ACTIVE)
+        print(self.status)
+        self.ID=str(uuid.uuid1())
         self._create_user_data()
     
     def _create_user_data(self):
@@ -21,6 +28,7 @@ class user:
             data = vars(self)
             json.dump(data,file,indent=4)
 
+    
 
-mahdi = user("mahdi@gmail.com","1234","mzz")
+mahdi = User("mahdi@gmail.com","1234","mzz")
 # print(mahdi)
