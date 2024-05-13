65bd4b926d367f1cb7a4b1f6e21a2baa81287ad3
thisismzz <mahdi.zeinal.zadeh.1384@gmail.com>
2024-05-13 11:38:34 +0330

    declare project class

--- board.py
+++ board.py
@@ -1,9 +1,41 @@
+import users
+from enum import Enum
+
+
+
 class Project:
-    def __init__(self,name,title):
+    def __init__(self,name,title,leader : users.User):
         self.name=name
-        self.
+        self.title=title
+        self.leader=leader
+        
+        
+    def add_member(self, user):
+        self.members.append(user)
+
+    def remove_member(self, user):
+        self.members.remove(user)
         
 
+class TaskStatus(Enum):
+    BACKLOG = 0
+    TODO = 1
+    DOING = 2
+    DONE = 3
+    ARCHIVED = 4
+    
+    def __str__(self):
+        return self.name
+
+class TaskPriority(Enum):
+    CRITICAL = 0
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+    
+    def __str__(self):
+        return self.name
 
 class Task:
-    def __init__(self)
+    def __init__(self,title,defenition,due,status=str(TaskStatus.BACKLOG),priority=str(TaskPriority.LOW)):
+        pass
