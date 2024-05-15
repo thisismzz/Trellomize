b775a5e4b03dfd346241772c739dbdd580d72fa5
KIMIA <149102812+KimiaBayat@users.noreply.github.com>
2024-05-15 18:24:01 +0330

    defining User class and add some functions validating username and email

--- main.py
+++ main.py
@@ -30,3 +30,49 @@ class Priority(Enum):
     HIGH = 'HIGH'
     MEDIUM = 'MEDIUM'
     LOW = 'LOW'
+    
+class User:
+    def __init__(self, email, username, password, active=True):
+        self.email = email
+        self.username = username
+        self.password = password
+        self.active = active
+
+    @staticmethod
+    def get_all_usernames():
+        data = load_data()
+        usernames = [user["username"] for user in data["users"]]
+        return usernames
+
+    @staticmethod
+    def validate_email_format(email):
+        email_regex = r'^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$'
+        return re.match(email_regex, email) is not None
+
+    @staticmethod
+    def validate_username_format(username):
+        username_regex = r'^[a-zA-Z0-9_]+$'
+        if re.match(username_regex, username) and 3 <= len(username) <= 20:
+            return True
+        return False
+
+    @staticmethod
+    def check_unique_username(username, data):
+        for user in data["users"]:
+            if user["username"] == username:
+                return False
+        return True
+
+    @staticmethod
+    def validate_password_strength(password):
+        if len(password) < 8:
+            return False
+        if not re.search(r"[A-Z]", password):
+            return False
+        if not re.search(r"[a-z]", password):
+            return False
+        if not re.search(r"[0-9]", password):
+            return False
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+            return False
+        return True
