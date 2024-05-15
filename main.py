bdbcf6ae277919bff8bfbe30963312540b82303e
KIMIA <149102812+KimiaBayat@users.noreply.github.com>
2024-05-15 18:22:39 +0330

    add enum classes to main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import json
+import os
+import uuid
+import bcrypt
+import re
+from enum import Enum
+from datetime import datetime, timedelta
+from rich.console import Console
+from rich.table import Table
+from rich.theme import Theme
+
+custom_theme = Theme({
+    "Title":  "bold Magenta",
+    "Info": "blue",
+    "Notice": "bold green",
+    "Error": "bold red"
+})
+
+console = Console(theme=custom_theme)
+
+class Status(Enum):
+    BACKLOG = 'BACKLOG'
+    TODO = 'TODO'
+    DOING = 'DOING'
+    DONE = 'DONE'
+    ARCHIVED = 'ARCHIVED'
+
+class Priority(Enum):
+    CRITICAL = 'CRITICAL'
+    HIGH = 'HIGH'
+    MEDIUM = 'MEDIUM'
+    LOW = 'LOW'
