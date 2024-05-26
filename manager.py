806f03f0a5af828caaafeeb5ab79f38e7a0068fb
thisismzz <mahdi.zeinal.zadeh.1384@gmail.com>
2024-05-26 19:51:18 +0330

    Revert "apply autopep8"
    This reverts commit 5f3596491cd7929d5bdcaee2179c85152e288992.

--- manager.py
+++ manager.py
@@ -19,54 +19,53 @@ console = Console(theme=custom_theme)
 
 logger = logging.getLogger("__manager__")
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler('logs.log', mode='a')
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler  = logging.FileHandler('logs.log' , mode='a')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
 class Manager:
-    def __init__(self, username, password):
+    def __init__(self , username , password):
         self.username = username
         self.password = password
-
+    
     def create_admin(self):
         if os.path.exists("manager_info.json"):
-            console.print("Error: Admin info already exists.", style='Error')
+            console.print("Error: Admin info already exists." , style='Error')
             exit()
-
+        
         admin_info = {
-            "username": self.username,
+            "username": self.username, 
             "password": base64.b64encode(self.password.encode("utf-8")).decode("utf-8")
         }
-
+        
         with open("manager_info.json", "w") as admin_file:
             json.dump(admin_info, admin_file)
         print("Admin info created successfully.")
         logger.info("Manager has created successfully")
-
+    
     def login(self):
         data = {}
         try:
-            with open("manager_info.json", 'r') as file:
+            with open("manager_info.json" , 'r') as file:
                 data = json.load(file)
-        except FileExistsError:
-            console.print("Manager is not defined", style='Error')
+        except FileExistsError : 
+            console.print("Manager is not defined" , style='Error')
             console.print("Create Manager first", style='Error')
             exit()
-
+            
         if data['username'] == self.username and base64.b64decode(data['password']).decode("utf-8") == self.password:
             console.print("Login successful.", style="Notice")
             logger.info("Manager has logged in successfully")
             self.manager_menu()
         else:
-            console.print("Invalid username or password.", style='Error')
+            console.print("Invalid username or password." , style='Error')
             exit()
-
+            
     def manager_menu(self):
         while True:
-            console.print(f"welcome Manager {self.username}", style='Title')
+            console.print(f"welcome Manager {self.username}" , style='Title')
             console.print("What would you like to do?", style="Info")
             console.print("1. Deactivate a user")
             console.print("2. Activated a user")
@@ -75,50 +74,47 @@ class Manager:
 
             choice = input("Enter your choice: ")
             if choice == '1':
-                console.print("Available users:", style='Info')
+                console.print("Available users:" , style='Info')
                 all_usernames = Manager.load_users()
                 for username in all_usernames:
                     console.print("-", username)
-                selected_usernames = list(map(lambda x: x.strip(), input(
-                    "Enter usernames to deactive (format:'user1,user2') or 'back' to go back: ").split(',')))
-
+                selected_usernames = list(map(lambda x:x.strip(),input("Enter usernames to deactive (format:'user1,user2') or 'back' to go back: ").split(',')))
+                
                 if selected_usernames[0] == 'back':
                     self.manager_menu()
-
+                
                 for username in selected_usernames:
                     Manager.deactive_user(username)
-
+                
             elif choice == '2':
-                console.print("Available users:", style='Info')
+                console.print("Available users:" , style='Info')
                 all_usernames = Manager.load_users()
                 for username in all_usernames:
                     console.print("-", username)
-                selected_usernames = list(map(lambda x: x.strip(), input(
-                    "Enter usernames to activate (format:'user1,user2') or 'back' to go back: ").split(',')))
-
+                selected_usernames = list(map(lambda x:x.strip(),input("Enter usernames to activate (format:'user1,user2') or 'back' to go back: ").split(',')))
+                
                 if selected_usernames[0] == 'back':
                     self.manager_menu()
-
+                
                 for username in selected_usernames:
                     Manager.activate_user(username)
-
+            
             elif choice == '3':
                 self.purge_data(is_run=True)
-
+            
             elif choice == '4':
-                console.print(
-                    "You have been successfully logged out.", style="Notice")
+                console.print("You have been successfully logged out.", style="Notice")
                 logger.info("End of Manager program")
                 exit()
 
             else:
                 console.print("Invalid choice.", style="Error")
-
+        
     def load_users():
         if not os.path.exists("emails and usernames.json"):
             print("Error: User info file not found.")
             return
-
+        
         with open("emails and usernames.json", "r") as user_file:
             users_data = json.load(user_file)
         return list(users_data["usernames"].values())
@@ -126,140 +122,132 @@ class Manager:
     def deactive_user(username):
         path = 'users/' + username + '/' + username + ".json"
         user_data = {}
-        with open(path, 'r') as file:
+        with open(path , 'r') as file :
             user_data = json.load(file)
-
+        
         if user_data["active"] == False:
-            console.print(
-                f"User ({username}) has already been deactivated", style='Error')
+            console.print(f"User ({username}) has already been deactivated" , style='Error')
             return
-
+        
         user_data["active"] = False
-
-        with open(path, 'w') as file:
-            json.dump(user_data, file, indent=4)
-
-        console.print(
-            f"User ({username}) has been deactivated successfully", style='Notice')
+        
+        with open (path , 'w') as file:
+            json.dump(user_data,file,indent=4)
+        
+        console.print(f"User ({username}) has been deactivated successfully", style='Notice')
         logger.info(f"User ({username}) deactivated by Manager")
 
     def activate_user(username):
         path = 'users/' + username + '/' + username + ".json"
         user_data = {}
-        with open(path, 'r') as file:
+        with open(path , 'r') as file :
             user_data = json.load(file)
-
+        
         if user_data["active"] == True:
-            console.print(f"User ({username}) is active", style='Error')
+            console.print(f"User ({username}) is active" , style='Error')
             return
-
+        
         user_data["active"] = True
-
-        with open(path, 'w') as file:
-            json.dump(user_data, file, indent=4)
-
-        console.print(
-            f"User ({username}) has been activated successfully", style='Notice')
+        
+        with open (path , 'w') as file:
+            json.dump(user_data,file,indent=4)
+        
+        console.print(f"User ({username}) has been activated successfully", style='Notice')
         logger.info(f"User ({username}) activated by Manager")
-
-    def purge_data(self, is_run=False):
+        
+        
+    def purge_data(self,is_run = False):
         if not is_run:
             data = {}
             try:
-                with open("manager_info.json", 'r') as file:
+                with open("manager_info.json" , 'r') as file:
                     data = json.load(file)
-            except FileExistsError:
-                console.print("Manager is not defined", style='Error')
+            except FileExistsError : 
+                console.print("Manager is not defined" , style='Error')
                 console.print("Create Manager first", style='Error')
                 exit()
 
             if data['username'] == self.username and base64.b64decode(data['password']).decode("utf-8") == self.password:
                 pass
             else:
-                console.print("Invalid username or password.", style='Error')
+                console.print("Invalid username or password." , style='Error')
                 exit()
-
+            
         project_path = 'projects/'
         with os.scandir(project_path) as entries:
             if not any(entries):
-                console.print("There is no project data", style='Error')
+                console.print("There is no project data" , style='Error')
             else:
                 shutil.rmtree(project_path)
-                console.print("All projects has been deleted", style='Notice')
+                console.print("All projects has been deleted" , style='Notice')
                 logger.info("All projects has been deleted")
                 os.makedirs(project_path)
-
+        
         users_path = 'users/'
         with os.scandir(users_path) as entries:
             if not any(entries):
-                console.print("There is no user data", style='Error')
+                console.print("There is no user data" , style='Error')
             else:
                 shutil.rmtree(users_path)
                 os.remove("emails and usernames.json")
-                console.print("All users has been deleted", style='Notice')
+                console.print("All users has been deleted" , style='Notice')
                 logger.info("All users has been deleted")
                 os.makedirs(users_path)
-                with open("emails and usernames.json", 'w') as file:
-                    data = {'emails': [], 'usernames': {}}
-                    json.dump(data, file, indent=4)
+                with open ("emails and usernames.json" , 'w') as file:
+                    data = {'emails' : [] , 'usernames' : {}}
+                    json.dump(data,file,indent=4)
 
 
 def get_username(ID):
     data = {}
-    try:
-        with open("emails and usernames.json", 'r') as file:
-            data = json.load(file)
+    try : 
+        with open ("emails and usernames.json" , 'r') as file:
+             data = json.load(file)
     except FileNotFoundError:
         logger.error("Problem with [emails and usernames.json]")
         raise FileNotFoundError("File Error. Terminating Program")
     return data["usernames"][ID]
-
-
+    
 def get_ID(username):
     data = {}
-    try:
-        with open("emails and usernames.json", 'r') as file:
-            data = json.load(file)
+    try : 
+        with open ("emails and usernames.json" , 'r') as file:
+             data = json.load(file)
     except FileNotFoundError:
         logger.error("Problem with [emails and usernames.json]")
         raise FileNotFoundError("File Error. Terminating Program")
-
+         
     for ID in data['usernames']:
         if data['usernames'][ID] == username:
-            return ID
+            return ID 
 
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="User and Admin Manager")
     subparsers = parser.add_subparsers(title="subcommands", dest="subcommand")
 
-    create_admin_parser = subparsers.add_parser(
-        "create-admin", help="Create admin credentials")
-    create_admin_parser.add_argument(
-        "--username", required=True, help="Admin username")
-    create_admin_parser.add_argument(
-        "--password", required=True, help="Admin password")
+    create_admin_parser = subparsers.add_parser("create-admin", help="Create admin credentials")
+    create_admin_parser.add_argument("--username", required=True, help="Admin username")
+    create_admin_parser.add_argument("--password", required=True, help="Admin password")
 
     login_parser = subparsers.add_parser("login", help="Log in as admin")
-    login_parser.add_argument(
-        "--username", required=True, help="Admin username")
-    login_parser.add_argument(
-        "--password", required=True, help="Admin password")
+    login_parser.add_argument("--username", required=True, help="Admin username")
+    login_parser.add_argument("--password", required=True, help="Admin password")
 
-    purge_parser = subparsers.add_parser("purge-data", help="Purge database")
-    purge_parser.add_argument(
-        "--username", required=True, help="Admin username")
-    purge_parser.add_argument(
-        "--password", required=True, help="Admin password")
-
+    purge_parser = subparsers.add_parser("purge-data" , help="Purge database")
+    purge_parser.add_argument("--username", required=True, help="Admin username")
+    purge_parser.add_argument("--password", required=True, help="Admin password")
+    
     args = parser.parse_args()
 
-    manager = Manager(args.username, args.password)
+    manager = Manager(args.username,args.password)
     if args.subcommand == "create-admin":
         manager.create_admin()
-
+        
     elif args.subcommand == "login":
         manager.login()
-
+    
     elif args.subcommand == "purge-data":
         manager.purge_data()
+        
--- test.py
+++ test.py
@@ -8,41 +8,37 @@ from main import User, Project, Task
 
 
 class TestMainClsUser(TestCase):
-
+    
     @classmethod
     def setUpClass(cls):
         cls.user1 = User("user1@test.com", "user1test", "user1", ID="tester")
-
+        
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(f"users/user1test")
-
+    
     def test_create_user(self):
         self.assertEqual(self.user1.email, 'user1@test.com')
         self.assertEqual(self.user1.username, 'user1test')
         self.assertEqual(self.user1.password, 'user1')
-
+        
     def test_user_file_validation(self):
         self.user1.save_user_data()
-        expected_data_user1 = {"email": "user1@test.com", "username": "user1test",
-                               "password": "user1", "active": True, "ID": "tester"}
-        self.assertTrue(os.path.exists(
-            f"users/{self.user1.username}/{self.user1.username}.json"))
-        self.assertEqual(expected_data_user1,
-                         User.load_user_data(self.user1.username))
+        expected_data_user1 = {"email": "user1@test.com", "username": "user1test", "password": "user1", "active": True, "ID": "tester"}
+        self.assertTrue(os.path.exists(f"users/{self.user1.username}/{self.user1.username}.json"))
+        self.assertEqual(expected_data_user1, User.load_user_data(self.user1.username))
 
 
 class TestMainClsProject(TestCase):
-
+    
     @classmethod
     def setUpClass(cls):
-        cls.project1 = Project("project test1", "user1",
-                               collaborators=None, ID="tester")
-
+        cls.project1 = Project("project test1", "user1", collaborators=None, ID="tester")
+    
     @classmethod
     def tearDownClass(cls):
         os.remove(f"projects/tester.json")
-
+    
     def test_create_project(self):
         self.assertEqual(self.project1.title, "project test1")
         self.assertEqual(self.project1.owner, "user1")
@@ -51,57 +47,54 @@ class TestMainClsProject(TestCase):
 
     def test_project_file_validation(self):
         self.project1.save_project_data()
-        expected_data_project = {"title": "project test1", "owner": "user1", "tasks": {
-        }, "collaborators": ["user1"], "ID": "tester"}
+        expected_data_project = {"title": "project test1", "owner": "user1", "tasks": {}, "collaborators": ["user1"], "ID": "tester"}
         self.assertTrue(os.path.exists("projects/tester.json"))
-        self.assertEqual(expected_data_project,
-                         Project.load_project_data("tester"))
+        self.assertEqual(expected_data_project, Project.load_project_data("tester"))
 
 
 class TestMainClsTask(TestCase):
-
+    
     def setUp(self):
-        self.task1 = Task("task1 test", "test No.1", ID="tester",
-                          start_time="2024-05-22 22:56:04.092445", end_time="2024-05-23 22:56:04.092445")
-
+        self.task1 = Task("task1 test", "test No.1", ID="tester", start_time="2024-05-22 22:56:04.092445", end_time="2024-05-23 22:56:04.092445")
+    
     def test_create_task(self):
-        self.assertEqual(self.task1.title, "task1 test")
-        self.assertEqual(self.task1.description, "test No.1")
-        self.assertEqual(self.task1.ID, "tester")
+        self.assertEqual(self.task1.title , "task1 test")
+        self.assertEqual(self.task1.description , "test No.1")
+        self.assertEqual(self.task1.ID , "tester")
         self.assertEqual(self.task1.start_time, "2024-05-22 22:56:04.092445")
-        self.assertEqual(self.task1.end_time, "2024-05-23 22:56:04.092445")
-        self.assertEqual(self.task1.priority, "LOW")
-        self.assertEqual(self.task1.status, "BACKLOG")
-
+        self.assertEqual(self.task1.end_time , "2024-05-23 22:56:04.092445")
+        self.assertEqual(self.task1.priority , "LOW")
+        self.assertEqual(self.task1.status , "BACKLOG")
+        
     @patch("builtins.input", side_effect=["2024-05-22 22:56:04"])
     def test_change_end_time(self, mock_input):
-        self.task1.change_end_time()
-        self.assertEqual(self.task1.end_time, "2024-05-22 22:56:04")
+        self.task1.change_end_time()  
+        self.assertEqual(self.task1.end_time , "2024-05-22 22:56:04")  
 
     @patch("builtins.input", side_effect=["2024-05-22 22:56:04"])
     def test_change_start_time(self, mock_input):
-        self.task1.change_start_time()
-        self.assertEqual(self.task1.start_time, "2024-05-22 22:56:04")
+        self.task1.change_start_time()  
+        self.assertEqual(self.task1.start_time , "2024-05-22 22:56:04")  
 
     @patch("builtins.input", side_effect=["2"])
     def test_change_status(self, mock_input):
-        self.task1.change_status()
-        self.assertEqual(self.task1.status, "TODO")
+        self.task1.change_status()  
+        self.assertEqual(self.task1.status , "TODO")  
 
     @patch("builtins.input", side_effect=["2"])
     def test_change_priority(self, mock_input):
-        self.task1.change_priority()
-        self.assertEqual(self.task1.priority, "HIGH")
+        self.task1.change_priority()  
+        self.assertEqual(self.task1.priority , "HIGH")  
 
     @patch("builtins.input", side_effect=["New Title"])
     def test_change_title(self, mock_input):
-        self.task1.change_title()
-        self.assertEqual(self.task1.title, "New Title")
+        self.task1.change_title()  
+        self.assertEqual(self.task1.title , "New Title")  
 
     @patch("builtins.input", side_effect=["New Description"])
     def test_change_description(self, mock_input):
-        self.task1.change_description()
-        self.assertEqual(self.task1.description, "New Description")
+        self.task1.change_description()  
+        self.assertEqual(self.task1.description , "New Description")  
 
     def test_using_mock(self):
         mock_obj = Mock()
